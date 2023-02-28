cba6a9d25dc5af88b9c4375468178e1798fe892f
maolululu <65891337+maolululu@users.noreply.github.com>
2023-02-28 12:47:49 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Found in .env.local
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = "https://mftdgmvaffmboupnnmbs.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mdGRnbXZhZmZtYm91cG5ubWJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM5NTQzNDYsImV4cCI6MTk4OTUzMDM0Nn0.kVUtqyAtEGNoF4aWep3ZGt9GFR8CN9MsN7n2O6xLzUs";
 // const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY as string;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
