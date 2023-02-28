74d8182794543b0abe05142bd7cd1b6d09e321fb
maolululu <65891337+maolululu@users.noreply.github.com>
2023-02-28 14:57:22 +0800

    Update dev.ts

--- lib/dev.ts
+++ lib/dev.ts
@@ -1,5 +1,5 @@
 export default function isDev() {
-    return process.env.NODE_ENV === "development";
+    return process.env.NODE_ENV === "production";
   }
   
   export const url = (path: string) => {
