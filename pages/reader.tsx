411fd7337c8c281b960ca7e09b4bd6b8b454fb60
maolululu <65891337+maolululu@users.noreply.github.com>
2023-02-28 21:24:13 +0800

    Update reader.tsx

--- pages/reader.tsx
+++ pages/reader.tsx
@@ -9,7 +9,7 @@ import React, {
   import { useRouter } from "next/router";
   import Head from "next/head";
   import axios from "axios";
-  axios.defaults.baseURL = 'http://47.113.194.193:3000';
+  axios.defaults.baseURL = 'https://47.113.194.193:3000';
   
   import { ChatFeed, Message } from "react-chat-ui";
   import {
